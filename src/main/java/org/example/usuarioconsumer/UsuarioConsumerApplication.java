28152771fa2c38ba2e2becd8e596f296943d8805
Daniel <daniellammer75@gmail.com>
2026-08-27 17:30:31 -0300

    chore: altera WebClient

--- src/main/java/org/example/usuarioconsumer/UsuarioConsumerApplication.java
+++ src/main/java/org/example/usuarioconsumer/UsuarioConsumerApplication.java
@@ -2,6 +2,12 @@ package org.example.usuarioconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.awt.*;
 
 @SpringBootApplication
 public class UsuarioConsumerApplication {
@@ -10,4 +16,17 @@ public class UsuarioConsumerApplication {
         SpringApplication.run(UsuarioConsumerApplication.class, args);
     }
 
+}
+
+@Bean
+public WebClient.Builder webClientBuilder(){
+    return WebClient.builder();
+}
+
+@Bean
+public WebClient webClient(WebClient.Builder builder){
+    return builder
+            .baseUrl("http://localhost:4000/")
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build();
 }
